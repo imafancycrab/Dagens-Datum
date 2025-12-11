baba73b00fdb769c07db96aa3d427ffa0862891e
industry <randompews@gmail.com>
2025-12-11 13:46:37 +0100

    added ticking time

--- index.js
+++ index.js
@@ -3,15 +3,40 @@ HTML.Month = document.getElementById("Month");
 HTML.Date = document.getElementById("Date");
 HTML.Year = document.getElementById("Year");
 HTML.Weekday = document.getElementById("Weekday")
+HTML.Time = document.getElementById("Time")
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i; }
+    return i;
+}
+
+function StartTime() {
+    const d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+
+    m = checkTime(m);
+    s = checkTime(s);
+
+    HTML.Time.textContent = h + ":" + m + ":" + s;
+
+    setTimeout(StartTime, 1000);
+}
 
 function LoadValues(){
     const d = new Date();
     const months = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
     const Days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Satrday","Sunday"];
-    HTML.Month.innerHTML = months[d.getMonth()];
-    HTML.Date.innerHTML = d.getDate()
-    HTML.Year.innerHTML = d.getFullYear();
-    HTML.Weekday.innerHTML = Days[d.getDay()]
+    HTML.Month.textContent = months[d.getMonth()];
+    HTML.Date.textContent = d.getDate();
+    HTML.Year.textContent = d.getFullYear();
+    HTML.Weekday.textContent = Days[d.getDay()];
+
+    StartTime();
 }
 
+
+
 LoadValues()
+
