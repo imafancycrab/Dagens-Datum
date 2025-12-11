ba92ff1a6a216b16fcad55bc38838dba8700b1e2
industry <randompews@gmail.com>
2025-12-11 13:25:50 +0100

    switched from api to local time

--- index.js
+++ index.js
@@ -4,29 +4,14 @@ HTML.Date = document.getElementById("Date");
 HTML.Year = document.getElementById("Year");
 HTML.Weekday = document.getElementById("Weekday")
 
-
-
-function GetIp(){
-    return fetch("https://api.ipify.org/")
-        .then(response => response.text());
-}
-
-async function GetTime(){
-    console.log("Fetching Time...")
-    const ip = await GetIp();
-    const response = await fetch("https://timeapi.io/api/time/current/ip?ipAddress=" + ip)
-    const data = await response.json();
-    console.log(data);
-    return data;
-}
-
-async function LoadValues(){
-    const data = await GetTime();
+function LoadValues(){
+    const d = new Date();
     const months = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
-    HTML.Month.innerHTML = months[data.month - 1];
-    HTML.Date.innerHTML = data.day;
-    HTML.Year.innerHTML = data.year;
-    HTML.Weekday.innerHTML = data.dayOfWeek;
+    const Days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Satrday","Sunday"];
+    HTML.Month.innerHTML = months[d.getMonth()];
+    HTML.Date.innerHTML = d.getDate()
+    HTML.Year.innerHTML = d.getFullYear();
+    HTML.Weekday.innerHTML = Days[d.getDay()]
 }
 
 LoadValues()
